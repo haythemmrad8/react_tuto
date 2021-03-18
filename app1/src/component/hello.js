78aa7f3981332835d4a5f4a4002b96858c489a51
haythemmrad8 <haythemmrad8@gmail.com>
2021-03-18 21:42:47 +0100

    010

--- app1/src/component/hello.js
+++ app1/src/component/hello.js
@@ -17,13 +17,26 @@ class Hello extends Component{
          dobleIT (){
             this.setState({classe: this.state.classe * 2 });
             }
+    updateName (event){
+        const newName = event.target.value;
+        this.setState({
+            user : {
+                ...this.state.user,
+                name:newName
+            }
+        })
+    }
     render(){
         return(
             <div>hello from {this.props.name} 
+            <p>{'name :'+ this.state.user.name}</p>
+            <p>{'age :'+ this.state.user.age}</p>
+
                 <p>{'classe : '+ this.state.classe }</p> 
                 <button onClick={()=>{this.setState({classe: this.state.classe + 1})}}>level UP </button>
                 <button onClick={this.levelDown}>level down</button>
                 <button onClick={this.dobleIT.bind(this)}>Double It</button>
+                <input type='text'onChange={this.updateName.bind(this)} ></input>
 
             </div>
         );
